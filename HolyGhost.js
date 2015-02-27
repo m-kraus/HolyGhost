b921708bbd956e427418340066e160353462c2b7
Michael Kraus <krausm@consol.de>
2015-02-27 15:56:43 +0100

    Prepare for 1.0 Code cleanup, documentation enhancements and preparation for simplehar

--- HolyGhost.js
+++ HolyGhost.js
@@ -1,25 +1,44 @@
-//TODO insert copyright and licensing
-//TODO add information that is script is compatible only with casperjs api v.1.1
+/*
+ * Copyright (c) 2013-2015 Michael Kraus
+ * 
+ * This file is part of HolyGhost.
+ * 
+ * HolyGhost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * HolyGhost is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses>.
+ */
 
 /*
  * This "pre"-"test" exhibits all his event processing and functions to the real test afterwards
  */
 
-
-
 /*
  * Format date as ISO string, needed to override builtin for correct HAR generation
  */
-Date.prototype.toISOString = function () {
-	function pad(n) { return n < 10 ? '0' + n : n; }
-	function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
-	return this.getFullYear() + '-' +
-		pad(this.getMonth() + 1) + '-' +
-		pad(this.getDate()) + 'T' +
-		pad(this.getHours()) + ':' +
-		pad(this.getMinutes()) + ':' +
-		pad(this.getSeconds()) + '.' +
-		ms(this.getMilliseconds()) + 'Z';
+Date.prototype.toISOString = function() {
+    function pad(n) {
+        return n < 10 ? '0' + n : n;
+    }
+
+    function ms(n) {
+        return n < 10 ? '00' + n : n < 100 ? '0' + n : n
+    }
+    return this.getFullYear() + '-' +
+        pad(this.getMonth() + 1) + '-' +
+        pad(this.getDate()) + 'T' +
+        pad(this.getHours()) + ':' +
+        pad(this.getMinutes()) + ':' +
+        pad(this.getSeconds()) + '.' +
+        ms(this.getMilliseconds()) + 'Z';
 };
 
 
@@ -28,221 +47,240 @@ Date.prototype.toISOString = function () {
  */
 var fs = require('fs');
 var utils = require('utils');
+//TODO shorten xpath selector like in check_casper
 
 /*
  * Get options from calling script
  */
-if ( casper.cli.has('hgResultpath') === false ) {
-	casper.die('Needed argument hgResultpath not provided');
+if (casper.cli.has('hgResultpath') === false) {
+    casper.die('Needed argument hgResultpath not provided');
 } else {
-	var resultpath = casper.cli.get("hgResultpath");
+    var resultpath = casper.cli.get("hgResultpath");
 }
 
 /*
  * Casper options
  */
 casper.options.viewportSize = {
-	width: 1024,
-	height: 768
+    width: 1024,
+    height: 768
 };
 casper.options.pageSettings = {
-	loadImages: true,
-	loadPlugins: false
+    loadImages: true,
+    loadPlugins: false
 };
 
 /*
+ * SetUp and TearDown
+ */
+// Set up: nothing
+casper.test.setUp(function() {
+    //unused
+});
+// Tear down:
+casper.test.tearDown(function() {
+    //unused
+    //casper.test.info("Clearing cookies");
+    //casper.page.clearCookies();
+});
+
+/*
  * Basic settings
-  */
+ */
 var pg = new WebPage();
 pg.resources = [];
 pg.startTime = new Date();
-pg.address = 'Start_'+pg.startTime.toISOString();
+pg.address = 'Start_' + pg.startTime.toISOString();
 casper.startTime = new Date();
 
 /*
  * Actions on test failures
  */
 casper.test.on("fail", function(failure) {
-	hgCapture(casper);
-	hgHar(casper);
-	casper.test.terminate("Aborted all remaining tests");
+    hgCapture(casper);
+    hgHar(casper);
+    casper.test.terminate("Aborted all remaining tests");
 });
+/*
+ * Actions on test completion
+ */
+// TODO complete or success
+if (casper.cli.get("hgDebug")) {
+    casper.test.on("complete", function() {
+        hgCapture(casper);
+    });
+}
+
 
 /*
  * Timeout options and handling
  */
 casper.options.timeout = 60000;
 casper.on('timeout', function() {
-	casper.test.fail('Test ran into Overall timeout');
+    casper.test.fail('Test ran into Overall timeout');
 });
 casper.options.waitTimeout = 10000;
 casper.on('waitFor.timeout', function() {
-	casper.test.fail('Test ran into WaitFor timeout');
+    casper.test.fail('Test ran into WaitFor timeout');
 });
 
 /*
  * HTTP status callbacks
  */
 casper.on("http.status.404", function(resource) {
-        casper.test.fail('HTTP Error 404');
+    casper.test.fail('HTTP Error 404');
 });
 casper.on("http.status.500", function(resource) {
-        casper.test.fail('HTTP Error 500');
+    casper.test.fail('HTTP Error 500');
 });
 
 /*
  * Load failed
  */
 casper.on("load.failed", function(resource) {
-        casper.test.fail('Page loading failed');
+    casper.test.fail('Page loading failed');
 });
 casper.on("error", function(msg, trace) {
-        casper.test.fail('Test failed with error' + msg);
+    casper.test.fail('Test failed with error' + msg);
 });
 
 /*
  * Funtion for capturing page html and screenshots
  */
 function hgCapture(casper) {
-	var now = new Date().toISOString();
-	// Save HTML
-	var html = casper.getHTML();
-	fs.write(resultpath+'/html__'+now+'.html', html, 'w');
-	// Take screenshots if enabled
-	if ( casper.cli.get("hgScreenshot") ) {
-		casper.capture(resultpath+'/screenshot__'+now+'.png');
-	}
-}
-/*
- * Funtion for getting har archive files
- */
+        var now = new Date().toISOString();
+        // Save HTML
+        var html = casper.getHTML();
+        fs.write(resultpath + '/html__' + now + '.html', html, 'w');
+        // Take screenshots 
+        casper.capture(resultpath + '/screenshot__' + now + '.png');
+    }
+    /*
+     * Funtion for getting har archive files
+     */
 function hgHar(casper) {
-	// Save HAR if enabled
-	if ( casper.cli.get("hgHar") ) {
-		var now = new Date().toISOString();
-		casper.endTime = new Date();
-		var content = JSON.stringify(createHar(pg.address, 'HolyGhost', pg.startTime, pg.resources), undefined, 3);
-		fs.write(resultpath+'/har.har', content, 'w');
-	}
+    // Save HAR
+    var now = new Date().toISOString();
+    casper.endTime = new Date();
+    var content = JSON.stringify(createHar(pg.address, 'HolyGhost', pg.startTime, pg.resources), undefined, 3);
+    fs.write(resultpath + '/har.har', content, 'w');
 }
 
 /*
- * HAR creation if enabled
+ * HAR creation
  */
-if ( casper.cli.get("hgHar") ) {
-	function createHar(address, title, startTime, resources)
-	{
-	    var entries = [];
-	    var now = new Date().toISOString();
-	
-	    resources.forEach(function (resource) {
-	        var request = resource.request,
-	            startReply = resource.startReply,
-	            endReply = resource.endReply;
+function createHar(address, title, startTime, resources) {
+    var entries = [];
+    var now = new Date().toISOString();
 
-	        if (!request || !startReply || !endReply ) {
-	            return;
-	        }
+    resources.forEach(function(resource) {
+        var request = resource.request,
+            startReply = resource.startReply,
+            endReply = resource.endReply;
 
-	        // Exclude Data URI from HAR file because
-	        // they aren't included in specification
-	        if (request.url.match(/(^data:image\/.*)/i)) {
-	        	return;
-	        }
-	
-	        entries.push({
-	            startedDateTime: request.time.toISOString(),
-	            time: endReply.time - request.time,
-	            request: {
-	                method: request.method,
-	                url: request.url,
-	                httpVersion: "HTTP/1.1",
-	                cookies: [],
-	                headers: request.headers,
-	                queryString: [],
-	                headersSize: -1,
-	                bodySize: -1
-	            },
-	            response: {
-	                status: endReply.status,
-	                statusText: endReply.statusText,
-	                httpVersion: "HTTP/1.1",
-	                cookies: [],
-	                headers: endReply.headers,
-	                redirectURL: "",
-	                headersSize: -1,
-	                bodySize: startReply.bodySize,
-	                content: {
-	                    size: startReply.bodySize,
-	                    mimeType: endReply.contentType
-	                }
-	            },
-	            cache: {},
-	            timings: {
-	                blocked: 0,
-	                dns: -1,
-	                connect: -1,
-	                send: 0,
-	                wait: startReply.time - request.time,
-	                receive: endReply.time - startReply.time,
-	                ssl: -1
-	            },
-	            pageref: address
-	        });
-	    });
-	
-	    return {
-	        log: {
-	            version: '1.2',
-	            creator: {
-	                name: "CasperJS",
-	                version: phantom.version.major + '.' + phantom.version.minor +
-	                    '.' + phantom.version.patch
-	            },
-	            pages: [{
-	                startedDateTime: startTime.toISOString(),
-	                id: address,
-	                title: title,
-	                pageTimings: {
-	                	onLoad: casper.endTime - casper.startTime
-	                }
-	            }],
-	            entries: entries
-	        }
-	    };
-	};
-	
-	casper.on('resource.requested', function (req, request) {
-		pg.resources[req.id] = {
-			request: req,
-			startReply: null,
-			endReply: null
-		};
-	});
-	
-	casper.on('resource.received', function(res) {
-		if (res.stage === 'start') {
-		pg.resources[res.id].startReply = res;
-		}
-		if (res.stage === 'end') {
-		pg.resources[res.id].endReply = res;
-		}
-	});
+        if (!request || !startReply || !endReply) {
+            return;
+        }
+
+        // Exclude Data URIs from HAR file because
+        // they aren't included in specification
+        if (request.url.match(/(^data:image\/.*)/i)) {
+            return;
+        }
+
+        entries.push({
+            startedDateTime: request.time.toISOString(),
+            time: endReply.time - request.time,
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: endReply.status,
+                statusText: endReply.statusText,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: endReply.headers,
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: startReply.bodySize,
+                content: {
+                    size: startReply.bodySize,
+                    mimeType: endReply.contentType
+                }
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: startReply.time - request.time,
+                receive: endReply.time - startReply.time,
+                ssl: -1
+            },
+            pageref: address
+        });
+    });
+
+    // TODO make sure phantom.X is set
+    return {
+        log: {
+            version: '1.2',
+            creator: {
+                name: "PhantomJS",
+                version: phantom.version.major + '.' + phantom.version.minor +
+                    '.' + phantom.version.patch
+            },
+            pages: [{
+                startedDateTime: startTime.toISOString(),
+                id: address,
+                title: title,
+                pageTimings: {
+                    onLoad: casper.endTime - casper.startTime
+                }
+            }],
+            entries: entries
+        }
+    };
 };
+
+casper.on('resource.requested', function(req, request) {
+    pg.resources[req.id] = {
+        request: req,
+        startReply: null,
+        endReply: null
+    };
+});
+
+casper.on('resource.received', function(res) {
+    if (res.stage === 'start') {
+        pg.resources[res.id].startReply = res;
+    }
+    if (res.stage === 'end') {
+        pg.resources[res.id].endReply = res;
+    }
+});
 
 /*
  * Actions on step completion
  */
 casper.on('step.complete', function(step) {
-	if (utils.isTruthy(this.getCurrentUrl())) {
-		pg.address = this.getCurrentUrl();
-	}
+    if (utils.isTruthy(this.getCurrentUrl())) {
+        pg.address = this.getCurrentUrl();
+    }
 });
 /*
  * Actions on test suite completion
  */
 casper.test.on("exit", function() {
-	hgHar(casper);
+    hgHar(casper);
 })
 
 /*
--- README.md
+++ README.md
@@ -1,28 +1,95 @@
-# HolyGhost
+# About HolyGhost
+
+HolyGhost is a monitoring solution for performing end2end-tests on websites (HTML/JavaScript). HolyGhost uses PhantomJS and CasperJS and therefore can run headless and with very low impact in multiple environments. HolyGhost has been tested on Linux and Mac OSX. HolyGhost is a plugin for nagios and similar monitoring tools like icinga, naemon or shinken.  
 
 ## Prerequisites
 
-To use HolyGhost phantomjs and casperjs have to be installed.
+To use HolyGhost phantomjs and casperjs have to be installed and reachable in your path.
+
+HolyGhost needs CasperJS API version 1.1+.
+
+**Please note, that at the time of writing (2015-02-16) PhantomJS 2.0.0 has been recently released. Unfortunately CasperJS works only when using the current master branch or the branch phantomjs-2 from its Github-repository. To make things a little more complictaed, there are no official PhantomJS-2 binaries for linux at the moment. You can download third party binaries from https://github.com/eugene1g/phantomjs/releases or compile it yourself.**
+
+**I recommend to use PhantomJS 1.9.7 and CasperJS 1.1-beta3 at the moment.**
+
+HolyGhost needs the perl modules XML::libXML and File::Which. On Ubuntu or Debian for exmaple you can install them with the following command:
+
+    sudo apt-get install libxml-libxml-perl libfile-which-perl
+
+PhantomJS 1.9 requires libfontconfig1. On Ubuntu or Debian you can install it with the following command:
+
+    sudo apt-get install libfontconfig1
 
 ### Installation under Linux
 
 Get phantomjs depending on your Architecture (32bit/64bit) from http://phantomjs.org/download.html
 
-    TODO
-    sudo ln -s /opt/phantomjs/.../phantomjs /usr/local/bin/
+    cd /opt
+    sudo tar xvf Downloads/phantomjs-1.9.8-linux-x86_64.tar
+    sudo ln -s phantomjs-1.9.8-linux-x86_64 phantomjs
+    sudo ln -s /opt/phantomjs/bin/phantomjs /usr/local/bin/
 
 Get casperjs from http://casperjs.org/
 
-    TODO
-    sudo ln -s /opt/casperjs/.../bin/casperjs /usr/local/bin/
+    cd /opt
+    sudo unzip Downloads/n1k0-casperjs-1.1-beta3-0-g4f105a9.zip
+    sudo ln -s n1k0-casperjs-4f105a9 casperjs
+    sudo ln -s /opt/casperjs/bin/casperjs /usr/local/bin/
+    
+## Write or record your tests
+
+A good starting point is the CasperJS documentation. See http://docs.casperjs.org/en/latest/ to get an idea of all the possibilities, CasperJS can offer you.
+
+You can also use the Chrome-extension "Resurrectio" to record CasperJS tests. You can find it on Github https://github.com/ebrehault/resurrectio or from the Chrome Web Store.
+
+Resurrectio can be a very good starting point to help you writing your tests, but be also aware, that from my experience no recorded test can suffice for qualified e2e montoring. For example you should really add more assertions or double-check, if Resurrectio chose the right css/xpath selectors for you.
+
+As you have to store passwords within your test case, you should really use dedicated accounts for your tests, that are restricted to the absolute minimal needed rights. Be also aware, that also your test results (screenshots, HAR-Files, HTML-Files, etc.) could expose confidential data.
+So be sure to make test cases and result files only accessible to trusted persons using file system permissions and webserver authentication. Also consider, whether notification texts could contain confidential data.
 
 ## Run your tests
+
+You can find some sample test cases in this repositories' subdirectory "tests".
 
 Run your tests simply specifiying your testcase and optionally your proxy.
 
     ./HolyGhost --test=tests/simpletest_casperjs.js --proxy=http://YOUR.PROXY.HERE:1234
 
-## Writing test cases
+For other options see
 
-See casperjs documentation as a starting point. See also the examples within the directory test/ to see basic layout of HolyGhost-tests.
-TODO: describe HolyGhost naming scheme
+    ./HolyGhost --help
+
+## HolyGhost conventions
+
+You can set CasperJS options within your test case, for example timeouts or viewport sizes:
+
+     casper.options.viewportSize = {width: 1600, height: 600};
+     casper.options.stepTimeout = 20000;
+     casper.options.waitTimeout = 10000;
+     casper.options.timeout = 60000;
+
+Very important for HolyGhost is the casper.test.begin statement.
+In the description string (first argument to casper.test.begin) you can set separated by :: the nagios return code (CRITICAL, WARNING, ...), a short name and a description of your test case.
+The "planned" number (second argument to casper.test.begin) represents the number of tests ("test.") used in your test case. You will get an error message from CasperJS if this number differs from the real number of tests within the test case.
+
+    casper.test.begin('CRITICAL::TestCaseName::Description of my test', 2, function suite( test ) { ...
+
+In the "message" string of a test you can set a step name and a step description, separated by ::.
+
+    test.assertTitle('Google', 'STEP1::Verify title is "Google"');
+
+Please note, that you can use the step name multiple times. Using this feature you can combine multiple test assertions into one single step. You get performace data for that combined step.
+
+In performance data outpu output, you also get the total execution time of the test case. Default label is "Total", you can change this with the command line option --label
+
+## Screenshots and HAR files
+
+HolyGhost captures always a cookiejar file and the HAR file of the complete test case. For more information about the HAR specification see https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/HAR/Overview.html
+
+HolyGhost also captures on test failure the current html source and a screenshot.
+
+By default these files are only kept on failed tests. You can change that behaviuor using the command line options --keep or --delete
+
+## HAR viewer
+
+TODO
